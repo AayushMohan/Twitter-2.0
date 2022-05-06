8b45123a5f53b0f0d675d2a9838220fa76720c14
AayushMohan <aayushmohan1702@gmail.com>
2022-05-06 23:27:09 +0530

    Added User Image in TweetBox

--- Components/TweetBox.tsx
+++ Components/TweetBox.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const TweetBox = () => {
-  return <div>TweetBox</div>
+  return (
+    <div>
+      <img
+        className="mt-4 h-14 w-14 rounded-full object-cover"
+        src="https://links.papareact.com/gll"
+        alt=""
+      />
+    </div>
+  )
 }
 
 export default TweetBox
