029b1ba597a5f432a84669b466bd8e26a6e62dd7
AayushMohan <aayushmohan1702@gmail.com>
2022-05-06 23:35:22 +0530

    Added Form in TweetBox

--- Components/TweetBox.tsx
+++ Components/TweetBox.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import {
+  CalendarIcon,
+  EmojiHappyIcon,
+  LocationMarkerIcon,
+  PhotographIcon,
+  SearchCircleIcon,
+} from '@heroicons/react/outline'
 
 const TweetBox = () => {
   return (
@@ -8,6 +15,23 @@ const TweetBox = () => {
         src="https://links.papareact.com/gll"
         alt=""
       />
+
+      <div>
+        <form>
+          <input type="text" placeholder="What's Happening?" />
+          <div>
+            <div className="flex space-x-2 text-twitter">
+              <PhotographIcon className="h-5 w-5" />
+              <SearchCircleIcon className="h-5 w-5" />
+              <EmojiHappyIcon className="h-5 w-5" />
+              <CalendarIcon className="h-5 w-5" />
+              <LocationMarkerIcon className="h-5 w-5" />
+            </div>
+
+            <button>Tweet</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
