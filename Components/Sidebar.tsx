59a5d20b26b91e4c80352528e255d64afd0be731
AayushMohan <aayushmohan1702@gmail.com>
2022-05-06 20:22:36 +0530

    Imported HeroIcons In SIdebar.tsx

--- Components/Sidebar.tsx
+++ Components/Sidebar.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from '@heroicons/react/outline'
 
 const Sidebar = () => {
-  return <div>I'm a sidebar</div>
+  return (
+    <div>
+      <img className="h-10 w-10" src="https://links.papareact.com/drq" alt="" />
+    </div>
+  )
 }
 
 export default Sidebar
