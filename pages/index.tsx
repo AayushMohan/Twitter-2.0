292f1ed58060cfa7e5bf6ea2250e1ff426bc6674
AayushMohan <aayushmohan1702@gmail.com>
2022-05-06 22:01:58 +0530

    Added Feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Feed from '../Components/Feed'
 import Sidebar from '../Components/Sidebar'
 
 const Home: NextPage = () => {
@@ -14,6 +15,7 @@ const Home: NextPage = () => {
         <Sidebar />
 
         {/* Feed */}
+        <Feed />
 
         {/* Widgets */}
       </main>
