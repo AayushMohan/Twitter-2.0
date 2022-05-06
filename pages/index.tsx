1d7026276c81556988f5b2fce9cb9b6b783aa48b
AayushMohan <aayushmohan1702@gmail.com>
2022-05-07 01:12:08 +0530

    Added FetchTweets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Feed from '../Components/Feed'
 import Sidebar from '../Components/Sidebar'
@@ -26,3 +26,11 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tweets = await fetchTweets()
+
+  return {
+    props: {},
+  }
+}
