62667c64e6ef416db4564659db4d22d6227110ce
AayushMohan <aayushmohan1702@gmail.com>
2022-05-07 17:10:01 +0530

    Added Comment Query

--- pages/api/getComments.ts
+++ pages/api/getComments.ts
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { groq } from 'next-sanity'
+
+const commentQuery = groq`
+ *[_type == "comment" && references(*[_type == 'tweet' && _id == $tweetId]._id)]{
+         _id,  
+         ...
+} | order(_createdAt desc)
+`
 
 type Data = {
   name: string
