4febfb235205ef2cc31f35e2606d5d3095a0c509
AayushMohan <aayushmohan1702@gmail.com>
2022-05-07 20:15:25 +0530

    Added Hr

--- Components/Tweet.tsx
+++ Components/Tweet.tsx
@@ -81,11 +81,12 @@ const Tweet = ({ tweet }: Props) => {
       {comments?.length > 0 && (
         <div className="my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5">
           {comments.map((comment) => (
-            <div key={comment._id} className="flex space-x-2">
+            <div key={comment._id} className="relative flex space-x-2">
+              <hr className="absolute left-5 top-10 h-8 border-x border-twitter/30" />
               <img
                 src={comment.profileImg}
                 alt=""
-                className="h-7 w-7 rounded-full object-cover"
+                className="mt-2 h-7 w-7 rounded-full object-cover"
               />
               <div>
                 <div className="flex items-center space-x-1">
