c117669d1db0f341effc9919c47b008d856fcd2e
AayushMohan <aayushmohan1702@gmail.com>
2022-06-02 21:38:38 +0530

    Added Comment Logic

--- Components/Tweet.tsx
+++ Components/Tweet.tsx
@@ -1,5 +1,7 @@
+// Tweet.tsx
+
 import React, { useEffect, useState } from 'react'
-import { Comment, Tweet } from '../typings'
+import { Comment, CommentBody, Tweet } from '../typings'
 import TimeAgo from 'react-timeago'
 import {
   ChatAlt2Icon,
@@ -7,18 +9,20 @@ import {
   SwitchHorizontalIcon,
   UploadIcon,
 } from '@heroicons/react/outline'
-import { fetchComments } from '../utils/fetchComments'
 import { useSession } from 'next-auth/react'
+import toast from 'react-hot-toast'
+import { fetchComments } from '../utils/fetchComments'
 
 interface Props {
   tweet: Tweet
 }
 
-const Tweet = ({ tweet }: Props) => {
+function Tweet({ tweet }: Props) {
+  const [commentBoxVisible, setCommentBoxVisible] = useState<boolean>(false)
+  const [input, setInput] = useState<string>('')
   const [comments, setComments] = useState<Comment[]>([])
+  const [likesCount, setLikesCount] = useState(0)
 
-  const [commentBoxIsVisible, setCommentBoxIsVisible] = useState(false)
-  const [input, setInput] = useState('')
   const { data: session } = useSession()
 
   const refreshComments = async () => {
@@ -30,16 +34,48 @@ const Tweet = ({ tweet }: Props) => {
     refreshComments()
   }, [])
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  // Like Button Logic
+  const handleLikeCountHandler = () => {
+    setLikesCount(likesCount + 1)
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+
+    const commentToast = toast.loading('Posting Comment...')
+
+    // Comment logic
+    const comment: CommentBody = {
+      comment: input,
+      tweetId: tweet._id,
+      username: session?.user?.name || 'Unknown User',
+      profileImg: session?.user?.image || 'https://links.papareact.com/gll',
+    }
+
+    const result = await fetch(`/api/addComment`, {
+      body: JSON.stringify(comment),
+      method: 'POST',
+    })
+
+    console.log('WOOHOO we made it', result)
+    toast.success('Comment Posted!', {
+      id: commentToast,
+    })
+
+    setInput('')
+    setCommentBoxVisible(false)
+    refreshComments()
   }
 
   return (
-    <div className="flex flex-col space-x-3 border-y border-gray-100 p-5">
+    <div
+      key={tweet._id}
+      className="flex flex-col space-x-3 border-y border-gray-100 p-5"
+    >
       <div className="flex space-x-3">
         <img
           className="h-10 w-10 rounded-full object-cover"
-          src={tweet.profileImg}
+          src={tweet.profileImg || 'https://links.papareact.com/gll'}
           alt=""
         />
 
@@ -47,7 +83,7 @@ const Tweet = ({ tweet }: Props) => {
           <div className="flex items-center space-x-1">
             <p className="mr-1 font-bold">{tweet.username}</p>
             <p className="hidden text-sm text-gray-500 sm:inline">
-              @{tweet.username.replace(/\s+/g, '').toLowerCase()} •
+              @{tweet.username.replace(/\s+/g, '').toLowerCase()} ·
             </p>
 
             <TimeAgo
@@ -55,54 +91,55 @@ const Tweet = ({ tweet }: Props) => {
               date={tweet._createdAt}
             />
           </div>
+
           <p className="pt-1">{tweet.text}</p>
 
           {tweet.image && (
             <img
               src={tweet.image}
+              className="m-5 ml-0 mb-1 max-h-60  rounded-lg object-cover shadow-sm"
               alt=""
-              className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm"
             />
           )}
         </div>
       </div>
+
       <div className="mt-5 flex justify-between">
         <div
-          onClick={() =>
-            session && setCommentBoxIsVisible(!commentBoxIsVisible)
-          }
-          className="flex cursor-pointer items-center space-x-3 text-gray-400 hover:text-twitter"
+          onClick={(e) => session && setCommentBoxVisible(!commentBoxVisible)}
+          className="flex cursor-pointer items-center space-x-3 text-gray-400 hover:text-blue-300"
         >
-          <ChatAlt2Icon className="h-8 w-8 rounded-full p-1 hover:bg-blue-100" />
-          <p>{comments.length}</p>
+          <ChatAlt2Icon className="h-8 w-8 rounded-full p-1.5 hover:bg-blue-50" />
+          <p className="hover:text-blue-300">{comments.length}</p>
         </div>
-
-        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
-          <SwitchHorizontalIcon className="h-5 w-5" />
+        <div className="flex cursor-pointer items-center space-x-3 rounded-full text-gray-400 hover:bg-green-100 hover:text-green-400">
+          <SwitchHorizontalIcon className="h-8 w-8 rounded-full p-1.5" />
         </div>
-
-        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
-          <HeartIcon className="h-5 w-5" />
+        <div
+          className="flex cursor-pointer items-center space-x-3 text-gray-400 hover:text-[#f91800]"
+          onClick={handleLikeCountHandler}
+        >
+          <HeartIcon className="h-8 w-8 rounded-full p-1.5 hover:bg-pink-100" />
+          <p>{likesCount}</p>
         </div>
-
-        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
-          <UploadIcon className="h-5 w-5" />
+        <div className="flex cursor-pointer items-center space-x-3 pr-1 text-gray-400 hover:text-blue-300">
+          <UploadIcon className="h-8 w-8 rounded-full p-1.5 hover:bg-blue-100" />
         </div>
       </div>
-      {/* Comment Box Logic */}
-      {commentBoxIsVisible && (
-        <form className="mt-3 flex space-x-3" onClick={handleSubmit}>
+
+      {commentBoxVisible && (
+        <form className="mt-3 flex space-x-3" onSubmit={handleSubmit}>
           <input
             value={input}
             onChange={(e) => setInput(e.target.value)}
             className="flex-1 rounded-lg bg-gray-100 p-2 outline-none"
             type="text"
-            placeholder="Write A Comment..."
+            placeholder="Write a comment..."
           />
           <button
             disabled={!input}
+            className="text-twitter disabled:text-gray-200"
             type="submit"
-            className="disabled text-twitter disabled:text-gray-200"
           >
             Post
           </button>
@@ -116,15 +153,16 @@ const Tweet = ({ tweet }: Props) => {
               <hr className="absolute left-5 top-10 h-8 border-x border-twitter/30" />
               <img
                 src={comment.profileImg}
-                alt=""
                 className="mt-2 h-7 w-7 rounded-full object-cover"
+                alt=""
               />
               <div>
                 <div className="flex items-center space-x-1">
                   <p className="mr-1 font-bold">{comment.username}</p>
                   <p className="hidden text-sm text-gray-500 lg:inline">
-                    @{comment.username.replace(/\s+/g, '').toLowerCase()} •
+                    @{comment.username.replace(/\s+/g, '').toLowerCase()} ·
                   </p>
+
                   <TimeAgo
                     className="text-sm text-gray-500"
                     date={comment._createdAt}
